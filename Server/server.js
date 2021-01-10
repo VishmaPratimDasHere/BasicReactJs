0c863edf3b5a3644f484e9f8dd13a863218e1f73
Vishma Pratim Das <bhishmadas9@gmail.com>
2021-01-10 21:14:36 +0530

    "Initialised server node package"

--- Server/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.listen(3100, () => {
-    console.log("API server started in port 3100!");
-});
-app.listen(3200, () => {
-    console.log("Second server started.")
-});
